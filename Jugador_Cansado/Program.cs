c9bca3679c08a7b2723c003c932dcbe50df7151d
mateo <mategiovannetti@gmail.com>
2025-06-03 13:53:18 -0300

    ttt

---
+++ Jugador_Cansado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Jugador_Cansado
+{
+    internal class Program
+    {
+        public class Jugador
+        {
+            //atributos
+            private int minutosRecorridos;
+
+            //contructor
+
+            //metodos
+            public bool correr(int minutos);
+            public bool cansado();
+            public void descansar(int minutos);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
