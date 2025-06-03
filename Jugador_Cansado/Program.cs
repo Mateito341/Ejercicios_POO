8f7c499bd44f2c90340f5c6c4104c32992d87643
mateo <mategiovannetti@gmail.com>
2025-06-03 20:38:50 -0300

    Ejercicio 3

--- Jugador_Cansado/Program.cs
+++ Jugador_Cansado/Program.cs
@@ -6,20 +6,70 @@ namespace Jugador_Cansado
     {
         public class Jugador
         {
-            //atributos
-            private int minutosRecorridos;
+            // Atributos
+            protected int minutosRecorridos;
+            protected int capacidadMaxima;
 
-            //contructor
+            // Constructor
+            public Jugador(int minutosRecorridos, int capacidadMaxima)
+            {
+                this.minutosRecorridos = minutosRecorridos;
+                this.capacidadMaxima = capacidadMaxima;
+            }
 
-            //metodos
-            public bool correr(int minutos);
-            public bool cansado();
-            public void descansar(int minutos);
+            // Métodos
+            public bool Correr(int minutos)
+            {
+                if ((minutosRecorridos + minutos) <= capacidadMaxima)
+                {
+                    minutosRecorridos += minutos;
+                    return true;
+                }
+                return false;
+            }
 
+            public bool Cansado()
+            {
+                return minutosRecorridos >= capacidadMaxima;
+            }
+
+            public void Descansar(int minutos)
+            {
+                minutosRecorridos -= minutos;
+                if (minutosRecorridos < 0)
+                {
+                    minutosRecorridos = 0;
+                }
+            }
         }
+
+        public class Amateur : Jugador
+        {
+            public Amateur(int minutosRecorridos) : base(minutosRecorridos, 20)
+            {
+            }
+        }
+
+        public class Profesional : Jugador
+        {
+            public Profesional(int minutosRecorridos) : base(minutosRecorridos, 40)
+            {
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Profesional miProfesional = new Profesional(0);
+            Amateur miAmateur = new Amateur(0);
+
+            Console.WriteLine("¿Profesional corrió 10 minutos? " + miProfesional.Correr(10));
+            Console.WriteLine("¿Está cansado? " + miProfesional.Cansado());
+
+            Console.WriteLine("¿Amateur corrió 25 minutos? " + miAmateur.Correr(25)); // Excede su capacidad
+            Console.WriteLine("¿Está cansado? " + miAmateur.Cansado());
+
+            miAmateur.Descansar(5);
+            Console.WriteLine("Descansó 5 minutos. ¿Ahora puede correr 15? " + miAmateur.Correr(15));
         }
     }
 }
